27a040c2e3fa9d9959f838499218a8a63376014d
Seria55 <itssseria5@gmail.com>
2023-02-11 22:47:15 -0600

    Add files via upload

--- config.js
+++ config.js
@@ -72,11 +72,14 @@ var guardTowers = [];
 var signs = [];
 deeds.push(new Deed('Aurora','Gaea','Gaea',1675474483281,false,4,641,1694,66,true,596,1674,666,1709,0));
 deeds.push(new Deed('Grukhanovill','Grukhan','Grukhan',1675674647878,false,4,615,1601,41,false,595,1596,623,1606,0));
-deeds.push(new Deed('Mystic Retreat','Peanut','Peanut',1675724177298,false,4,1464,398,44,false,1424,343,1504,438,5));
+deeds.push(new Deed('Mystic Retreat','Peanut','Peanut',1675724177298,false,4,1464,398,44,false,1412,343,1515,441,0));
 deeds.push(new Deed('Nirvuston','Nirvus','Nirvus',1675737775248,false,4,663,3510,103,false,643,3490,683,3530,0));
 deeds.push(new Deed('Portobelo','Kilem','Kilem',1675792074275,false,4,2836,689,240,false,2786,639,2886,739,0));
 deeds.push(new Deed('Art Of Zen','Zenity','Zenity',1675802758198,false,4,3370,633,182,false,3365,628,3380,638,0));
-deeds.push(new Deed('Hot Sauce Central','Frank','Frank',1675903902733,false,4,369,2423,106,false,364,2418,374,2428,0));
+deeds.push(new Deed('Hot Sauce Central','Frank','Frank',1675903902733,false,4,369,2423,106,false,363,2417,381,2429,0));
+deeds.push(new Deed('Wolfmere','Wolfdangler','Wolfdangler',1676057052473,false,4,882,931,3,false,862,911,902,941,0));
+deeds.push(new Deed('Peachstone','Zelan','Zelan',1676059323098,false,4,703,624,156,false,693,614,713,634,0));
+deeds.push(new Deed('Sleepy Town','Tinashunter','Tinashunter',1676089981423,false,4,416,2481,154,false,406,2471,426,2491,0));
 kingdoms[0] = new Kingdom(0,'no known kingdom','','[color-list]');
 kingdoms[1] = new Kingdom(1,'Jenn-Kellon','','');
 kingdoms[2] = new Kingdom(2,'Mol Rehan','','');
@@ -88,4 +91,4 @@ bridgeNodes = [
 ];
 tunnelNodes = [
 ];
-var timestamp = 1675989964277;
+var timestamp = 1676176869308;
